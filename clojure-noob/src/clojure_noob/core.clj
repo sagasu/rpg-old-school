2427dd4437932f225ebbc0cc29cebbd9cf6c659f
Mateusz Kopij <sagasu@gmail.com>
2020-10-29 21:13:14 +0000

    more clojure

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,4 +4,9 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (+ 1 2 3) ; this will not be printed
+  (str "go " "go and " "again go") ;neither this
+  (if true
+  (println "By Zeus's hammer!")
+  (println "By Aquaman's trident! I am else"))) 
